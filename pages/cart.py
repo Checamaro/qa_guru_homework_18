35ab23aff2d28c5fa514bef4be6bce32e10aec93
checamar0 <checamar02@gmail.com>
2024-05-25 13:13:43 +0300

    Initial commit

---
+++ pages/cart.py
@@ -0,0 +1,23 @@
+import allure
+from selene import browser
+from allure import attach
+from allure_commons.types import AttachmentType
+import requests
+
+class Cart:
+    def open_to_cart(self):
+        with allure.step('Open cart'):
+            browser.open('/cart')
+            return self
+
+    @staticmethod
+    def open_cart_with_products(url=None, data=None):
+        with allure.step('Open cart with products'):
+            result = requests.post(url=url, data=data)
+            cookies = result.cookies.get('Nop.customer')
+            attach(body=cookies, name='cookies', attachment_type=AttachmentType.TEXT)
+            browser.open('https://demowebshop.tricentis.com/cart')
+            browser.driver.add_cookie({'name': 'Nop.customer', 'value': cookies})
+
+# Инициализация экземпляра класса Cart
+add_products_to_cart = Cart()
